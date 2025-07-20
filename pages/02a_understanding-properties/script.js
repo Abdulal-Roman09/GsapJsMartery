4a2dadadce4249912aa0a0a76a1e661b9bd227fa
Abdulal-noman <mdroman45678910@gamil.com>
2025-07-20 15:31:05 +0600

    2nd lesson

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,19 @@
+import gsap from "gsap";
+
+gsap.to('.box',{
+    opacity:1,
+    rotate:360,
+    repeat:-1,
+    duration:2,
+    // background:"#ff6f61f",
+    // borderRadius:'50%',
+    scale:1.25,
+    ease:'bounce',
+    yoyo:true,
+    repeatDelay:2,
+    // paused:true,
+    // stagger:1,it is work with gro s 
+
+
+
+})
